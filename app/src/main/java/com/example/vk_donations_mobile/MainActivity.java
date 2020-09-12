4bee83edcae6ad8da5f730f409cb257e05c686df
Nikita Shidlovsky <nik-801@yandex.ru>
2020-09-12 13:12:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/vk_donations_mobile/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.vk_donations_mobile;
+import android.content.Intent; // подключаем класс Intent
+import android.view.View; // подключаем класс View для обработки нажатия кнопки
+import android.widget.EditText; // подключаем класс EditText
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
